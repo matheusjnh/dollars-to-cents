3a1a663f0b5f1d5a733d0bc68f061b4497080f38
Matheus Santos <matheusjnh@gmail.com>
2023-02-24 18:30:36 -0300

    create dollar input logic

--- screens/Home/Home.tsx
+++ screens/Home/Home.tsx
@@ -1,15 +1,30 @@
-import { Text, View, StatusBar } from 'react-native';
+import { Text, View, StatusBar, TextInput } from 'react-native';
 
 import { useFonts, Inter_700Bold } from '@expo-google-fonts/inter';
 
 import { styles } from './styles';
+import { useState } from 'react';
 
 export function Home() {
+  const [dollarValue, setDollarValue] = useState(0);
+  const [error, setError] = useState(false);
+
   const [fontsLoaded] = useFonts({
     inter_bold: Inter_700Bold,
   });
 
   if (!fontsLoaded) return null;
+
+  const handleOnTextChange = (text: string) => {
+    const valueInNumber = Number(text);
+
+    if (Number.isNaN(valueInNumber)) {
+      setError(true);
+    } else {
+      setDollarValue(valueInNumber);
+      setError(false);
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -19,6 +34,16 @@ export function Home() {
         backgroundColor="transparent"
         translucent
       />
+
+      <View style={styles.inputArea}>
+        <Text style={styles.dollarSymbol}>$</Text>
+
+        <TextInput
+          style={[styles.input, error && styles.inputError]}
+          onChangeText={handleOnTextChange}
+          keyboardType="decimal-pad"
+        ></TextInput>
+      </View>
     </View>
   );
 }
--- screens/Home/styles.ts
+++ screens/Home/styles.ts
@@ -1,17 +1,47 @@
 import { StyleSheet } from 'react-native';
 
+const inputAreaFontSize = 30;
+
+const theme = {
+  textPrimary: '#252621',
+  textSecondary: '#25262173',
+};
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 60,
-    paddingHorizontal: 20,
+    paddingHorizontal: 40,
     backgroundColor: '#E8E9C7',
     alignItems: 'center',
   },
 
   title: {
-    color: '#252621',
-    fontSize: 38,
+    paddingBottom: 30,
+    color: theme.textPrimary,
+    fontSize: 35,
     fontFamily: 'inter_bold',
+  },
+
+  inputArea: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    fontSize: inputAreaFontSize,
+  },
+
+  dollarSymbol: {
+    paddingRight: 10,
+    color: theme.textSecondary,
+    fontSize: inputAreaFontSize,
+  },
+
+  input: {
+    fontSize: inputAreaFontSize,
+    borderBottomColor: theme.textSecondary,
+    borderBottomWidth: 2,
+  },
+
+  inputError: {
+    borderBottomColor: '#F52447',
   },
 });
